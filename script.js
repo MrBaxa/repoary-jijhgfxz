4aa114b364d5fe2f3ab133e72f183df3558bae59
MrBaxa <maripovbahti@gmail.com>
2025-02-28 16:12:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let mmm = prompt("Text kiriting");
+
+let fon = document.getElementById("text");
+    fon.innerHTML = mmm;
+    fon.style.color = "blue";
+    fon.style.width = "100vw"; 
+    fon.style.height = "100vh";
+    fon.style.display = "flex"; 
+    fon.style.justifyContent = "center"; 
+    fon.style.alignItems = "center"; 
+    fon.style.fontSize = "50px"; 
+    fon.style.backgroundColor = `green`
